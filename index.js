b3cb41218287ca13bae729dff0f370bccf857134
matata samuel <mattasamuel3@gmail.com>
2020-06-29 20:20:28 +0300

    adding all files

---
+++ index.js
@@ -0,0 +1,48 @@
+// navigation slide function
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // toggle Nav
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+       
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }   
+  
+        });
+
+        //toggle animation
+        burger.classList.toggle('toggle');
+    });
+    
+}
+navSlide();
+
+// scroll to top function
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
